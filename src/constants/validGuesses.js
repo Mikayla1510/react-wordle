7abee140e7941c68ca4185b89d5ac35eeffe5800
KaylaG1510 <mikayla.piercy@numberworks.com>
2024-12-24 13:22:31 +1300

    Update validGuesses.js

--- src/constants/validGuesses.js
+++ src/constants/validGuesses.js
@@ -1,7 +1,7 @@
 // Generate all 4-digit combinations from "0000" to "9999"
-const VALID_GUESSES = Array.from({ length: 10000 }, (_, i) => {
+export const VALID_GUESSES = Array.from({ length: 10000 }, (_, i) => {
   return i.toString().padStart(4, '0');
 });
 
 // Export the list
-export { VALID_GUESSES };
+// export { VALID_GUESSES };
